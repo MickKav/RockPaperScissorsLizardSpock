95963a5608e27742b19dbba08e9776d109179d52
MickKav <>
2023-07-20 02:45:42 +0000

    Add the logic for user selection and response

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,32 @@ let triesLeft = 3;
 function computerChoice() {
      // how many choices are there
      const lengthOfChoices = choices.length;
-     
+
      // get a random number between 0 and the number of choices
-     const computersChoice = choices[Math.floor(Math.random() * lengthOfChoices)];
+     const computersChoice = choices[Math.floor(Math.random() * lengthOfChoices)]; 
+     
+     // return the random choice
      return computersChoice;
 }
 
-function playRound() {}
+function playRound(playerChoice) {
+    const computer = computerChoice();
+    let result = "";
+
+    if (playerChoice === computer) {
+        result = "It's a tie!";
+    } else if (
+        (playerChoice === "rock" && (computer === "scissors" || computer === "lizard")) ||
+        (playerChoice === "scissors" && (computer === "paper" || computer === "lizard")) ||
+        (playerChoice === "paper" && (computer === "rock" || computer === "spock")) ||
+        (playerChoice === "lizard" && (computer === "paper" || computer === "spock")) ||
+        (playerChoice === "spock" && (computer === "rock" || computer === "scissors"))
+    ) {
+        score++;
+        result = `You win! ${playerChoice} beats ${computer}.`;
+    } else {
+        triesLeft--;
+        result = `You lose! ${computer} beats ${playerChoice}.`;
+    }
+}
 
