b06c4f37f38015f42bad4d584c5c9789d2d07428
MickKav <>
2023-07-20 02:28:55 +0000

    Create random return value for computerChoice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,18 @@ let score = 0;
 let triesLeft = 3;
 
 
-function computerChoice() {}
+/**
+  * Computer choice
+  * Returns a random choice from the choices array.
+  */
+function computerChoice() {
+     // how many choices are there
+     const lengthOfChoices = choices.length;
+     
+     // get a random number between 0 and the number of choices
+     const computersChoice = choices[Math.floor(Math.random() * lengthOfChoices)];
+     return computersChoice;
+}
 
 function playRound() {}
 
