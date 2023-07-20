b2889c37509b90dc1a647185212e7df1a20ef638
MickKav <>
2023-07-20 02:20:02 +0000

    Initialize game layout in js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// List of button choices to select
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+// User score status
+let score = 0;
+
+// Amount of tries left in game
+let triesLeft = 3;
+
+
+function computerChoice() {}
+
+function playRound() {}
+
